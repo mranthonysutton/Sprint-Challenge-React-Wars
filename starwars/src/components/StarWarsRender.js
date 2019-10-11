9571be29e215bf95e69686bade88303a6a45dc30
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-11 10:16:35 -0700

    Added styles to buttons and button container

--- starwars/src/components/StarWarsRender.js
+++ starwars/src/components/StarWarsRender.js
@@ -7,6 +7,7 @@ const StarWarsRender = props => {
   const [personData, setPersonData] = useState([]);
   const [pageControl, setPageControl] = useState(1);
 
+  // Styled Components
   const FlexContainer = styled.div`
     display: flex;
     align-items: center;
@@ -14,6 +15,32 @@ const StarWarsRender = props => {
     justify-content: space-around;
   `;
 
+  const ButtonContainer = styled.div`
+    background-color: rgba(81, 87, 81, 0.6);
+    color: #fff;
+    width: 30%;
+    padding: 2rem;
+    margin: 1rem auto;
+    box-shadow: 1px 2px 5px #63585e;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  `;
+
+  const PaginationButtons = styled.button`
+    padding: 3%;
+    cursor: pointer;
+    border: 1px solid #ebebeb;
+    border-radius: 5px;
+    background: #52489c;
+    color: #ebebeb;
+    font-size: 1.2rem;
+
+    &:hover {
+      box-shadow: 1px 3px 2px #52489c;
+  `;
+
+  // Axios API data fetch
   useEffect(() => {
     axios
       .get(`https://swapi.co/api/people/?page=${pageControl}`)
@@ -28,10 +55,15 @@ const StarWarsRender = props => {
 
   return (
     <>
-      <>
-        <button onClick={() => setPageControl(pageControl - 1)}>Prev</button>
-        <button onClick={() => setPageControl(pageControl + 1)}>Next</button>
-      </>
+      <ButtonContainer>
+        <h1>Page: {pageControl}</h1>
+        <PaginationButtons onClick={() => setPageControl(pageControl - 1)}>
+          Prev
+        </PaginationButtons>
+        <PaginationButtons onClick={() => setPageControl(pageControl + 1)}>
+          Next
+        </PaginationButtons>
+      </ButtonContainer>
       <FlexContainer>
         {personData.map((person, index) => {
           return (
