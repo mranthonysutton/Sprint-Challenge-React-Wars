d71d304e5aa24119918d21e00cd27bd8de1b742b
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-11 09:21:05 -0700

    Added basic styling to cards

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,16 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+
+const CharacterCardWrapper = styled.div`
+  background-color: rgba(81, 87, 81, 0.6);
+  color: #fff;
+  width: 40%;
+  padding: 2rem;
+  margin: 1rem 0;
+  box-shadow: 1px 2px 5px #63585e;
+`;
+
+const NameTitle = styled.h1`
+  @import url("https://fonts.googleapis.com/css?family=Mansalva&display=swap");
+  font-family: "Mansalva", cursive;
+  font-size: 3.5rem;
+`;
+
+const DescriptionElements = styled.p`
+  @import url("https://fonts.googleapis.com/css?family=Grenze&display=swap");
+  font-family: "Grenze", serif;
+  font-size: 2rem;
+  margin: 0;
+  padding: 0;
+  color: #eee;
+`;
 
 const StarWarsCard = props => {
   return (
-    <div className="starwars-card" key={props.name}>
-      <h1>Name: {props.name}</h1>
-      <ul>
-        <li>Gender: {props.gender}</li>
-        <li>Hair Color: {props.hairColor}</li>
-        <li>Skin Color: {props.skinColor}</li>
-        <li>Eye Color: {props.eyeColor}</li>
-      </ul>
-    </div>
+    <CharacterCardWrapper key={props.name}>
+      <NameTitle>{props.name}</NameTitle>
+      <DescriptionElements>Gender: {props.gender}</DescriptionElements>
+      <DescriptionElements>Hair Color: {props.hairColor}</DescriptionElements>
+      <DescriptionElements>Skin Color: {props.skinColor}</DescriptionElements>
+      <DescriptionElements>Eye Color: {props.eyeColor}</DescriptionElements>
+    </CharacterCardWrapper>
   );
 };
 
--- starwars/src/components/StarWarsRender.js
+++ starwars/src/components/StarWarsRender.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import StarWarsCard from "./StarWarsCard";
+import styled from "styled-components";
 
 const StarWarsRender = props => {
   const [personData, setPersonData] = useState([]);
+
+  const FlexContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  `;
 
   useEffect(() => {
     axios
@@ -18,10 +26,7 @@ const StarWarsRender = props => {
   }, []);
 
   return (
-    <div className="container">
-      {/* {personData.map((person, index) => {
-        return <StarWarsCard key={index} name={person} />;
-      })} */}
+    <FlexContainer>
       {personData.map((person, index) => {
         return (
           <StarWarsCard
@@ -34,7 +39,7 @@ const StarWarsRender = props => {
           />
         );
       })}
-    </div>
+    </FlexContainer>
   );
 };
 
